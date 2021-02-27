6453602c7f1f4f82f00dc3af69a3fd6a8198afad
Vinicius Lopes Simoes <vini.lopes.simoes@gmail.com>
2021-02-27 16:06:57 -0300

    Fix trollface image

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './Header.css';
 
+import trollface_img from "../../Assets/Trollface.png";
+
 function Header() {
   return (
     <header>
         <img 
-            src="././Assets/Trollface.png" 
-            alt="Problem?"
+            src={trollface_img}
+            alt="Problem???"
         />
         <p>Meme Generator</p>
     </header>
