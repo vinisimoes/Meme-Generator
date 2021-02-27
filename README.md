ac37e410ab9411d19ad4cb95562c2933de2d947e
Vinicius Lopes Simoes <vini.lopes.simoes@gmail.com>
2021-02-27 16:41:11 -0300

    Updates

--- README.md
+++
@@ -1,5 +0,0 @@
-# Meme Generator
-
-* Link: https://vinisimoes.github.io/Meme-Generator/
-
-![](meme-generator.gif)
---
+++ precache-manifest.c3f05cf0d2460661c4d969c2fa91d05b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7da912830f376f5377ce91224cf2c80b",
+    "url": "/Meme-Generator/index.html"
+  },
+  {
+    "revision": "684f294f1d5378692080",
+    "url": "/Meme-Generator/static/css/main.39f7b32d.chunk.css"
+  },
+  {
+    "revision": "7b186d13bdfa7fb810db",
+    "url": "/Meme-Generator/static/js/2.cff70578.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Meme-Generator/static/js/2.cff70578.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "684f294f1d5378692080",
+    "url": "/Meme-Generator/static/js/main.b0ad749e.chunk.js"
+  },
+  {
+    "revision": "a24f7684e08eece56398",
+    "url": "/Meme-Generator/static/js/runtime-main.cc46d329.js"
+  },
+  {
+    "revision": "be324d5b695be0894732e4e02e8fcc9f",
+    "url": "/Meme-Generator/static/media/Trollface.be324d5b.png"
+  }
+]);
